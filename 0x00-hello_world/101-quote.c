e85139b333bac192a28779eb6a7efc4c9bd6acf9
Onweli <michaelonweli@gmail.com>
2022-12-11 08:01:27 -0800

    my first line on C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 
 /**
  * main - print exactly "and that piece of art is useful"
- * Dora Korpar, 2015-10-19
+ * Dora Korpar, 2015-10-19"
  * followed by a new line, to the standard error.
  * Return: Always 0 (success)
  */
