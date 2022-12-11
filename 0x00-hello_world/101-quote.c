b56f5b4e5ba7debe822cd908f9ed54dcc4b39776
Onweli <michaelonweli@gmail.com>
2022-12-11 08:05:03 -0800

    my first line on C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - print exactly "and that piece of art is useful"
+ * main - prints exactly "and that piece of art is useful"
  * Dora Korpar, 2015-10-19"
  * followed by a new line, to the standard error.
  * Return: Always 0 (success)
