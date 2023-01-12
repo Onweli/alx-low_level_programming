1cf8ab00a126cc5a9695fe8840f6f88ba4488665
onweli <michaelonweli@gmail.com>
2023-01-12 10:55:02 -0800

    task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,7 +8,7 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _puts(char *str);
 int _putchar(char c);
-int -atoi(const char *s);
+int _atoi(const char *s);
 void print_int(unsigned long int n);
 
 
