a97119fe8e51e82201f5125a21b22e702ae59894
onweli <michaelonweli@gmail.com>
2023-01-12 11:27:04 -0800

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,28 +1,19 @@
-include "main.h"
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * create_array - creates an array of chars,
- * and initializes it with a specific char.
- * @size: the size of the array
- * @c: initial value
+ * malloc_checked - cause normal process termination with a status value of 98
  *
- * Return: a pointer to the array, or NULL if it fails
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
  */
-char *create_array(unsigned int size, char c)
+void *malloc_checked(unsigned int b)
 {
-	char *array;
-	unsigned int i;
+	void *i;
 
-	if (size == 0)
-		return (NULL);
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
 
-	array = (char *)malloc(sizeof(char) * size);
-	if (array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		array[i] = c;
-
-	return (array);
 }
