d132b499ea0fe11bc8cbb0f0dbc565d293d3e96f
onweli <michaelonweli@gmail.com>
2023-01-19 09:58:25 -0800

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int _putchar(char c);
+int _putchar(char *c);
 
-#endif
+#endif /*FUNCTION_POINTERS_H/
 
